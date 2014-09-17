f4973e7d02174e3e6b9e0e47e402ea0cd44b681a
Tony DiCola <tony@tonydicola.com>
2014-09-17 21:02:33 +0000

    Update example to include fahrenheit.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -29,6 +29,10 @@ import time
 import Adafruit_MCP9808.MCP9808 as MCP9808
 
 
+# Define a function to convert celsius to fahrenheit.
+def c_to_f(c):
+	return c * 9.0 / 5.0 + 32.0
+
 # Default constructor will use the default I2C address (0x18) and pick a default I2C bus.
 #
 # For the Raspberry Pi this means you should hook up to the only exposed I2C bus
@@ -48,5 +52,6 @@ sensor.begin()
 # Loop printing measurements every second.
 print 'Press Ctrl-C to quit.'
 while True:
-	print 'Temperature: {0:0.4F} *C'.format(sensor.readTempC())
+	temp = sensor.readTempC()
+	print 'Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp))
 	time.sleep(1.0)
