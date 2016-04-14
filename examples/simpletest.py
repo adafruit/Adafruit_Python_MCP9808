52166a70fc7ab171b8c1689f560150f708e153a0
Tony DiCola <tony@tonydicola.com>
2016-04-14 19:56:11 +0000

    Small tweak to example for python 3 compatibility.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -50,8 +50,8 @@ sensor = MCP9808.MCP9808()
 sensor.begin()
 
 # Loop printing measurements every second.
-print 'Press Ctrl-C to quit.'
+print('Press Ctrl-C to quit.')
 while True:
 	temp = sensor.readTempC()
-	print 'Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp))
+	print('Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
 	time.sleep(1.0)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name              = 'Adafruit_MCP9808',
-      version           = '1.5.0',
+      version           = '1.5.1',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Library for accessing the MCP9808 precision temperature sensor on a Raspberry Pi or Beaglebone Black.',
