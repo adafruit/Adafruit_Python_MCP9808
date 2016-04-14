9b04abf8ae809ea5fc72b0094b93d0b6246b102f
Tony DiCola <tony@tonydicola.com>
2016-04-14 19:58:43 +0000

    Add pypi metadata.

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@ from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
 
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
 setup(name              = 'Adafruit_MCP9808',
       version           = '1.5.1',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Library for accessing the MCP9808 precision temperature sensor on a Raspberry Pi or Beaglebone Black.',
       license           = 'MIT',
+      classifiers       = classifiers,
       url               = 'https://github.com/adafruit/Adafruit_Python_MCP9808/',
       dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
       install_requires  = ['Adafruit-GPIO>=0.6.5'],
